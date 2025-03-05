ee387d4df3ca3c7e80e8cb43b63f6a3449046e43
Rina <rina.newhouse@gmail.com>
2025-03-05 16:51:30 -0500

    new commit

--- books.js
+++ books.js
@@ -1,4 +1,36 @@
+function renderBooks() {
+  const booksWrapper = document.querySelector(".books")
 
+  const books = getBooks()
+
+  const booksHtml = books.map((book) => {
+    return `<div class="book">
+        <figure class="book__img--wrapper">
+          <img class="book__img" src="${book.url}" alt="">
+        </figure>
+        <div class="book__title">
+          ${books.title}
+        </div>
+        <div class="book__ratings">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star-half-alt"></i>
+        </div>
+        <div class="book__price">
+          <span class="book__price--normal">$${book.originalPrice}</span> $${book.salesPrice}
+        </div>
+      </div>`
+  })
+
+  booksWrapper.innerHTML = booksHtml
+  console.log(booksHtml)
+}
+
+setTimeout(() => {
+  renderBooks
+})
 
 // FAKE DATA
 function getBooks() {
@@ -6,7 +38,7 @@ function getBooks() {
     {
       id: 1,
       title: "Crack the Coding Interview",
-                url: "assets/crack the coding interview.png",
+      url: "assets/crack the coding interview.png",
       originalPrice: 49.95,
       salePrice: 14.95,
       rating: 4.5,
